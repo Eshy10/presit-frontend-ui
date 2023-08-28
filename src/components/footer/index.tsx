3de0f4933d3a768040b0e6057495ce2087c46d36
Eshy10 <adewalebose15@gmail.com>
2023-08-28 11:44:16 +0100

    fix: styling

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -6,22 +6,30 @@ import FooterStyles from './footer.module.css';
 const Footer = () => {
    return (
       <footer className={FooterStyles.footer}>
-         <div>
+         <div className={FooterStyles.footerLeft}>
             <img src={Logo} alt="logo" />
-            <p>
+            <p className={FooterStyles.footerContent}>
                Social media validation business model canvas graphical user interface launch party
                creative facebook iPad twitter.
             </p>
-            <p>All rights reserved.</p>
+            <p className={FooterStyles.footerReserved}>All rights reserved.</p>
          </div>
-         <div>
+         <div className={FooterStyles.footerRight}>
             {MENU_FOOTER.map((menu, index) => (
                <div key={index} className={FooterStyles.footerMenuWrapper}>
                   <h3>{menu.name}</h3>
                   {menu.menu.map((subMenu, index) => (
-                     <Link to={subMenu.link} key={index} className={FooterStyles.footerMenu}>
+                     <Link
+                        to={subMenu.link}
+                        key={index}
+                        className={`${FooterStyles.footerMenu}  ${
+                           subMenu.name === 'Careers' ? FooterStyles.footerMenuCareer : null
+                        }`}
+                     >
                         {subMenu.name}
-                        {menu.tag && <span className={FooterStyles.tag}>Hiring</span>}
+                        {menu.tag && subMenu.name === 'Careers' && (
+                           <span className={FooterStyles.tag}>Hiring</span>
+                        )}
                      </Link>
                   ))}
                </div>
--- src/pages/blog/Blog.tsx
+++ src/pages/blog/Blog.tsx
@@ -1,34 +1,39 @@
 import { Button, NewsCard, BlogCta } from 'components';
 import { LATEST_NEWS } from 'data';
 import { BirdBackground, BingUser } from 'images';
+import BlogStyles from './blog.module.css';
 
 const Blog = () => {
    return (
       <main>
-         <section>
-            <p>Blog</p>
-            <h2>Thoughts and Words</h2>
-            <div>
-               <img src={BirdBackground} alt="" />
-               <div>
-                  <div>
-                     <p>Category</p>
-                     <p>November 22, 2021</p>
+         <section className={BlogStyles.BlogHeader}>
+            <p className={BlogStyles.BlogHeaderTitle}>Blog</p>
+            <h2 className={BlogStyles.BlogHeaderSub}>Thoughts and Words</h2>
+            <div className={BlogStyles.BlogHeaderContent}>
+               <img src={BirdBackground} alt="" className={BlogStyles.BlogHeaderContentImage} />
+               <div className={BlogStyles.BlogHeaderContentRight}>
+                  <div className={BlogStyles.BlogHeaderDateWrapper}>
+                     <p className={BlogStyles.BlogHeaderCategory}>Category</p>
+                     <p className={BlogStyles.BlogHeaderContentDate}>November 22, 2021</p>
                   </div>
-                  <p>Pitch termsheet backing validation focus release.</p>
-                  <div>
-                     <img src={BingUser} alt="" />
-                     <p>Chandler Bing</p>
+                  <p className={BlogStyles.BlogHeaderContentText}>
+                     Pitch termsheet backing validation focus release.
+                  </p>
+                  <div className={BlogStyles.BlogHeaderAuthorWrapper}>
+                     <img src={BingUser} alt="" className={BlogStyles.BlogHeaderAuthorImage} />
+                     <p className={BlogStyles.BlogHeaderAuthorName}>Chandler Bing</p>
                   </div>
                </div>
             </div>
          </section>
-         <section>
+         <section className={BlogStyles.BlogNewsWrapper}>
             <h2>Latest News</h2>
-            {LATEST_NEWS.map((news, index) => (
-               <NewsCard news={news} key={index} />
-            ))}
-            <Button className="" outline="black">
+            <div className={BlogStyles.BlogNewsContent}>
+               {LATEST_NEWS.map((news, index) => (
+                  <NewsCard news={news} key={index} />
+               ))}
+            </div>
+            <Button className={BlogStyles.cardLoadMore} outline="black">
                Load more
             </Button>
          </section>
